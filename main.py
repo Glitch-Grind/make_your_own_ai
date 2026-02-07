48c41b64b808798da686838dd0b62e52c6eaca8a
Gunavardhan <yajjalainfi@gmail.com>
2026-02-07 16:14:17 +0530

    Add core project structure

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+from core.brain import respond
+from safety.checker import is_safe
+
+
 def main():
-	print("Starting this project...")
-	print("Status: Pre Alpha")
+    user_input = input("You: ")
+
+    if not is_safe(user_input):
+        print("AI: Input rejected by safety system.")
+        return
+
+    reply = respond(user_input)
+    print("AI:", reply)
+
 
 if __name__ == "__main__":
-	main()
+    main()
